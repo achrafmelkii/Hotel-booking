19d182a5a0d1ebdaaf51bc0293b36a2dd9f1b42d
Achraf MLK <achraf.melkii@gmail.com>
2021-07-30 23:58:11 +0100

    intial commit ,created routing module , added hotel pages aded paths

---
+++ src/app/weddings/foreign-weddings/foreign-weddings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foreign-weddings',
+  templateUrl: './foreign-weddings.component.html',
+  styleUrls: ['./foreign-weddings.component.css']
+})
+export class ForeignWeddingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/weddings/tunisian-weddings/tunisian-weddings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tunisian-weddings',
+  templateUrl: './tunisian-weddings.component.html',
+  styleUrls: ['./tunisian-weddings.component.css']
+})
+export class TunisianWeddingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
