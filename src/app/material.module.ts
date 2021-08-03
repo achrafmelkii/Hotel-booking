e45222a8017a2de19ab3a6e531a61c2e8c1d520f
Achraf MLK <achraf.melkii@gmail.com>
2021-08-03 09:58:31 +0100

    added home component and navigation components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from "@angular/core";
 import { MatSliderModule } from '@angular/material/slider';
 import { MatBadgeModule} from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
 @NgModule({
     imports:[
         MatSliderModule,
-        MatBadgeModule,
-        MatButtonModule
+        MatBadgeModule, 
+        MatButtonModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        FlexLayoutModule,
+        MatTabsModule
+       
     ],
     exports:[
         MatSliderModule,    
         MatBadgeModule,
-        MatButtonModule ]
+        MatButtonModule,
+        MatSidenavModule ,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        FlexLayoutModule,
+        MatTabsModule  
+    ]
 })
 export class materialModule{}
